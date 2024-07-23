43a8a760fad0fcccfb17f651479c81fd28839729
Owen HENRY <owenhenry59@outlook.fr>
2024-07-23 20:24:07 +0200

    init

--- script.js
+++ script.js
@@ -1,72 +1,20 @@
-$(document).ready(function() {
-        
-        $(".nav-button").on('click', function(event) {
-            if (this.hash !== "") {
-                event.preventDefault();
-                
-                let hash = this.hash;
-                
-                $('html, body').animate({
-                    scrollTop: $(hash).offset().top
-                }, 2000, function(){
-                    window.location.hash = hash;
-                });
-            }
-        });
-    
-        
-        $('.fade-in').each(function() {
-            var element = $(this);
-            element.waypoint(function() {
-                element.addClass('appear');
-            }, { offset: '80%' });
-        });
+$(document).ready(function(){
+    $('.projet-slider').bxSlider({
+        mode: 'fade',
+        captions: true,
+        controls: true,
+        pager: true
     });
-
-    
-    let modal = document.getElementById('modal');
-    let modalOkBtn = document.getElementById('modal-ok-btn');
-    let closeModal = document.getElementsByClassName('close')[0];
-
-    modal.style.display = "block"; // Afficher la modal au chargement de la page
-
-    modalOkBtn.onclick = function() {
-        modal.style.display = "none"; // Cacher la modal quand on clique sur OK
-    }
-
-    closeModal.onclick = function() {
-        modal.style.display = "none"; // Cacher la modal quand on clique sur le bouton de fermeture
-    }
-
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none"; // Cacher la modal si on clique en dehors de la modal
-        }
-    }
-
-    const form = document.getElementById('contactForm');
-const responseDiv = document.getElementById('formResponse');
-
-form.addEventListener('submit', function(event) {
-    event.preventDefault(); // Empêche le rechargement de la page
-
-    const formData = new FormData(form);
-
-    // Pour voir les données dans la console (pour le débogage)
-    for (let [key, value] of formData.entries()) {
-        console.log(key, value);
-    }
-
-    // Simule une soumission réussie
-    responseDiv.innerHTML = 'Merci, ' + formData.get('name') + '! Votre message a été envoyé.';
-
-    
-    form.reset();
 });
-    
-    
-    
 
-
-
-
+$(document).ready(function() {
+    $('nav ul a').click(function(event) {
+        event.preventDefault();
+        var target = $(this.hash);
+        if (target.length) {
+            $('html, body').animate ({
+                scrollTop: target.offset().top
+            }, 2000);
+        }
+    });
+});
